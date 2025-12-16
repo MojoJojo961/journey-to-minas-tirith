3f189964c53eb748954ff656f599ad3e960a239b
Sumit Chakraborty <sumit.chakraborty961@gmail.com>
2025-12-16 11:36:10 +0000

    Create Scene.jsx

---
+++ src/components/Scene.jsx
@@ -0,0 +1,8 @@
+export default function Scene({ next }) {
+  return (
+    <section className="scene">
+      <h1>Welcome, Brave Traveler</h1>
+      <button onClick={next}>Begin Journey</button>
+    </section>
+  );
+}
