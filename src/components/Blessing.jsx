1dbc12df47f9d2dfed7f0bb5bcc415a03b1a496a
Sumit Chakraborty <sumit.chakraborty961@gmail.com>
2025-12-16 11:37:56 +0000

    Create Blessing.jsx

---
+++ src/components/Blessing.jsx
@@ -0,0 +1,9 @@
+export default function Blessing({ next }) {
+  return (
+    <div className="scene">
+      <img src="/images/ganesh.png" alt="Lord Ganesh" />
+      <h2>Blessings from the Divine</h2>
+      <button onClick={next}>RSVP</button>
+    </div>
+  );
+}
