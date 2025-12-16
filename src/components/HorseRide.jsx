975c0146c7d20ec9369895c30923535948b046a1
Sumit Chakraborty <sumit.chakraborty961@gmail.com>
2025-12-16 13:59:32 +0000

    Update HorseRide.jsx

--- src/components/HorseRide.jsx
+++ src/components/HorseRide.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF, Html } from "@react-three/drei";
 
 export default function HorseRide({ next }) {
-  const { scene: horse } = useGLTF("/models/horse.glb");
-
   return (
-    <div className="horse-ride">
-      <Canvas camera={{ position: [0, 5, 20] }}>
-        <ambientLight intensity={0.6} />
-        <directionalLight position={[10, 10, 10]} />
-        <primitive object={horse} />
-        <OrbitControls enableZoom={false} />
-      </Canvas>
-      <Html>
-        <button onClick={next}>Ride to Minas Tirith</button>
-      </Html>
+    <div className="horse-ride scene">
+      <img src="/images/horse.png" alt="Horse Ride" style={{ width: "80%", maxWidth: 300 }} />
+      <button onClick={next}>Ride to Minas Tirith</button>
     </div>
   );
 }
