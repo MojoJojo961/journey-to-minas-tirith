3fa0aa02d5a6f39da92c7948685629a4491d3037
Sumit Chakraborty <sumit.chakraborty961@gmail.com>
2025-12-16 14:01:10 +0000

    Update SceneWrapper.jsx

--- src/components/SceneWrapper.jsx
+++ src/components/SceneWrapper.jsx
@@ -8,7 +8,8 @@ export default function SceneWrapper({ children }) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 1 }}
+      transition={{ duration: 0.8 }}
+      style={{ textAlign: "center", padding: "20px" }}
     >
       {children}
     </motion.div>
