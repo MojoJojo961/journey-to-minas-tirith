a6b1ac900fba0f261a871884640ffafd8648d272
Sumit Chakraborty <sumit.chakraborty961@gmail.com>
2025-12-16 14:00:21 +0000

    Update WeddingScene.jsx

--- src/components/WeddingScene.jsx
+++ src/components/WeddingScene.jsx
@@ -1,23 +1,16 @@
-import React, { useEffect } from "react";
-import { Howl } from "howler";
+import React from "react";
 import QRCode from "react-qr-code";
 
-export default function WeddingScene() {
-  useEffect(() => {
-    const frodo = new Howl({ src: ["/audio/frodo.mp3"] });
-    const shehnai = new Howl({ src: ["/audio/shehnai.mp3"] });
-    frodo.play();
-    shehnai.play();
-  }, []);
-
+export default function WeddingScene({ next }) {
   return (
-    <div className="wedding-scene">
-      <h1 className="elvish-font">Aarav & Leela</h1>
+    <div className="wedding-scene scene">
+      <h1>Aarav & Leela</h1>
       <p>14 December 2026</p>
-      <img src="/images/dancers.png" alt="Bollywood Dancers" />
-      <img src="/images/ganesh.png" alt="Lord Ganesh" />
-      <img src="/images/fireworks.png" alt="Fireworks" />
+      <img src="/images/dancers.png" alt="Dancers" style={{ width: "60%", maxWidth: 250 }} />
+      <img src="/images/ganesh.png" alt="Lord Ganesh" style={{ width: "50%", maxWidth: 200 }} />
+      <img src="/images/fireworks.png" alt="Fireworks" style={{ width: "80%", maxWidth: 300 }} />
       <QRCode value="https://your-rsvp-link.com" size={180} />
+      <button onClick={next}>RSVP</button>
     </div>
   );
 }
