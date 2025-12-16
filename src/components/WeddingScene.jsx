39918aa487e3a5279fedf6deb695b17b58ba7281
Sumit Chakraborty <sumit.chakraborty961@gmail.com>
2025-12-16 14:16:12 +0000

    Update WeddingScene.jsx

--- src/components/WeddingScene.jsx
+++ src/components/WeddingScene.jsx
@@ -1,16 +1,67 @@
 import React from "react";
+import { motion } from "framer-motion";
 import QRCode from "react-qr-code";
 
 export default function WeddingScene({ next }) {
   return (
-    <div className="wedding-scene scene">
-      <h1>Aarav & Leela</h1>
-      <p>14 December 2026</p>
-      <img src="/images/dancers.png" alt="Dancers" style={{ width: "60%", maxWidth: 250 }} />
-      <img src="/images/ganesh.png" alt="Lord Ganesh" style={{ width: "50%", maxWidth: 200 }} />
-      <img src="/images/fireworks.png" alt="Fireworks" style={{ width: "80%", maxWidth: 300 }} />
-      <QRCode value="https://your-rsvp-link.com" size={180} />
-      <button onClick={next}>RSVP</button>
-    </div>
+    <motion.div
+      className="scene"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h1
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        Aarav & Leela
+      </motion.h1>
+      <motion.p
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5 }}
+      >
+        14 December 2026
+      </motion.p>
+
+      <motion.img
+        src="/images/dancers.png"
+        alt="Dancers"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 1, duration: 1 }}
+      />
+
+      <motion.img
+        src="/images/ganesh.png"
+        alt="Lord Ganesh"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 1.5, duration: 1 }}
+      />
+
+      <motion.img
+        src="/images/fireworks.png"
+        alt="Fireworks"
+        className="fireworks"
+      />
+
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ delay: 2 }}
+      >
+        <QRCode value="https://your-rsvp-link.com" size={180} />
+      </motion.div>
+
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        onClick={next}
+        style={{ marginTop: "30px" }}
+      >
+        RSVP
+      </motion.button>
+    </motion.div>
   );
 }
